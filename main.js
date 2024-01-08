8c47c3edc9507a61ac4250be21fc769a0fc24918
CoderInProgress2 <119031303+CoderInProgress2@users.noreply.github.com>
2024-01-08 18:25:32 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var first_name = "Shayan";
+var last_name = "Rahman";
+var concat = first_name.concat(last_name);
+console.log(first_name);
+console.log(last_name);
+console.log(concat)
+var a = 10;
+var b = 5;
+var multiply = a + b;
+console.log(multiply)
